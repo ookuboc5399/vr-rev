44f1252d709ecf42be4bc7e08264c1e96fb6eb38
ookuboc5399 <ankle0614@gmail.com>
2021-06-19 00:09:47 +0900

    0618

--- app/models.py
+++ app/models.py
@@ -23,6 +23,14 @@ class Blog(models.Model):
 	image = models.ImageField(upload_to='images', verbose_name='イメージ画像', null=True, blank=True)
 	content = models.TextField("本文")
 	created = models.DateTimeField("作成日", default=timezone.now)
+category = models.CharField(max_length=255,default='VR')
 
-	def __str__(self):
-		return self.title
+def __str__(self):
+        return self.title
+
+
+class Caregory(models.Model):
+    name=models.CharField(max_length=255)
+
+def __str__(self):
+    return self.title
