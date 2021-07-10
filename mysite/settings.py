bba4a6ae2b5424273d6fbc7977880caf4424453f
ookuboc5399 <ankle0614@gmail.com>
2021-07-10 13:46:48 +0900

    complete

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,3 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST_USER = 'xxx@gmail.com'
 # EMAIL_HOST_PASSWORD = 'xxx'
 # EMAIL_USE_TLS = True
+
+import django_heroku　# 追加
+
+# 一番下に追加
+django_heroku.settings(locals())
