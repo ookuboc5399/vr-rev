ff676b6689c9e1a00e5f2f7ab879af92eaba0fe6
ookuboc5399 <ankle0614@gmail.com>
2023-08-12 19:40:57 +0900

    superuser.pyを変更

--- app/management/commands/superuser.py
+++ app/management/commands/superuser.py
@@ -7,9 +7,8 @@ User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username=settings.SUPERUSER_NAME).exists():
+        if not User.objects.filter(email=settings.SUPERUSER_NAME).exists():
             User.objects.create_superuser(
-                username=settings.SUPERUSER_NAME,
                 email=settings.SUPERUSER_EMAIL,
                 password=settings.SUPERUSER_PASSWORD
             )
